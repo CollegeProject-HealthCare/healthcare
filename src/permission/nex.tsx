2c44bb21955ac32fa1b9b8a8359959b3ae57d041
mdsams <tabrejmdsams786@gmail.com>
2023-01-23 20:51:17 +0530

    new try

---
+++ src/permission/nex.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const nex = () => {
+  return (
+    <View>
+      <Text>nex</Text>
+    </View>
+  );
+};
+
+export default nex;
